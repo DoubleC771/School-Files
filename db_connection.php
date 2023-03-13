b559f7272a899f43fa8b21b6f483edd246b6a77b
OuroK <drevanill777@gmail.com>
2023-03-13 12:10:32 +0800

    change back

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 function OpenCon()
  {
  $dbhost = "localhost";
- $dbuser = "nigus";
+ $dbuser = "root";
  $dbpass = "";
  $db = "logintest";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
