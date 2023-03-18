f2bd497a0f89d711986736babb02e814932eb31d
Luc1l1us <ikazuha001@gmail.com>
2023-03-19 01:02:05 +0800

    added SQL table for ease of import

---
+++ WebDev/EcommerceWebsite/sql/logintest.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2023 at 06:01 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logintest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bikeorder`
+--
+
+CREATE TABLE `bikeorder` (
+  `OrderID` int(11) NOT NULL,
+  `BikeName` varchar(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Image` varchar(50) NOT NULL,
+  `Price` decimal(10,0) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Stocks` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bikeorder`
+--
+
+INSERT INTO `bikeorder` (`OrderID`, `BikeName`, `Quantity`, `Image`, `Price`, `Description`, `Stocks`) VALUES
+(0, 'XMX 4000', 0, 'Bike4.JPG', '500', 'idk wat to do', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `OrderID` int(255) NOT NULL,
+  `BikeName` varchar(255) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `Image` varchar(255) NOT NULL,
+  `Quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintable`
+--
+
+CREATE TABLE `logintable` (
+  `CustomerID` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `UserType` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logintable`
+--
+
+INSERT INTO `logintable` (`CustomerID`, `firstname`, `lastname`, `address`, `city`, `username`, `password`, `UserType`) VALUES
+(1, 'Edzie Mari', 'Navarra', '1234', 'Bacolod City', 'deez', 'deez', 1),
+(11, 'martin', 'daguia', '2131', 'Bacolod', 'hein', 'hein', 2),
+(16, 'heinrich', 'imperial', '1234', 'dasdasd', 'hein', 'hein', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `logintable`
+--
+ALTER TABLE `logintable`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `OrderID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `logintable`
+--
+ALTER TABLE `logintable`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
