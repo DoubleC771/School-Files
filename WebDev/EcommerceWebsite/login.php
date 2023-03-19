bc905cb53c7d2566c781fd968d09b8ca5341ad79
Luc1l1us <ikazuha001@gmail.com>
2023-03-19 22:53:59 +0800

    changed login/signup pages to be better looking

--- WebDev/EcommerceWebsite/login.php
+++ WebDev/EcommerceWebsite/login.php
@@ -31,31 +31,34 @@ include("db_connection.php");
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;600&display=swap" rel="stylesheet"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>Login</title>
+	  <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	  <link rel="stylesheet" type="text/css" href="./css/bootstrap-icons.css">
 </head>
 <body>
     <form method="POST">
-    <div class = "form-container" id = "table-container">
-
-    <?php 
-         if ($is_invalid):
-      ?>
-      <em style = "color:red">Invalid Login</em>
-      <?php endif; ?>
-
-    <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="text" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username" name = "username" value = "<?= htmlspecialchars($_POST['username'] ?? "")?>">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your username with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control" id="password" placeholder="Password" name = "password">
-  </div>
-  <div class="form-check">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</div> 
-</div>
-</form>
+    <div class="col-md-4 border rounded mx-auto mt-5 p-4 shadow">
+			
+			<div class="h2">Login</div>
+			<div><small class="my-1 js-error js-error-email text-danger"></small></div>
+			<div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+			  <input name="username" type="text" class="form-control p-3" placeholder="Username" >
+			</div>
+			<div class="input-group mb-3">
+			  <span class="input-group-text" id="basic-addon1"><i class="bi bi-key"></i></span>
+			  <input name="password" type="password" class="form-control p-3" placeholder="Password" >
+			</div>
+			<div class="progress my-3 d-none">
+			  <div class="progress-bar" role="progressbar" style="width: 50%;" >Working... 25%</div>
+			</div>
+			<button class="btn btn-primary col-12">Login</button>
+			<div class="m-2">
+				Dont have an account? <a href="signup.php">signup here</a>
+			</div>
+		</div>
+    </form>
 </body>
 </html>
