37d4f7ca99124fd7c6363a034e9679060eb7916f
OuroK <drevanill777@gmail.com>
2023-03-19 23:55:52 +0800

    front end

--- WebDev/EcommerceWebsite/login.php
+++ WebDev/EcommerceWebsite/login.php
@@ -28,6 +28,7 @@ include("db_connection.php");
 	<title>PHP Form</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="login.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;600&display=swap" rel="stylesheet"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -41,7 +42,7 @@ include("db_connection.php");
     <div class = "bodybg2">
 </div>
     <form method="POST">
-    <div class="col-md-4 border rounded mx-auto mt-5 p-4 shadow">
+    <div class="row col-md-4 border rounded mx-auto mt-5 p-4 shadow">
 			
 			<div class="h2">Login</div>
 			<div><small class="my-1 js-error js-error-email text-danger"></small></div>
--- WebDev/EcommerceWebsite/signup.php
+++ WebDev/EcommerceWebsite/signup.php
@@ -33,7 +33,8 @@ include("db_connection.php");
 <html>
 <head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="login.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;600&display=swap" rel="stylesheet"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -44,8 +45,10 @@ include("db_connection.php");
 	  <link rel="stylesheet" type="text/css" href="./css/bootstrap-icons.css">
 </head>
 <body>
+	<div class = "bodybg2">
+</div>
     <form method="post" action="">
-    <div class="col-md-8 col-lg-4 border rounded mx-auto mt-5 p-4 shadow">
+    <div class="row col-md-8 col-lg-4 border rounded mx-auto mt-5 p-4 shadow">
 			
 			<div class="h2">Signup</div>
 			
