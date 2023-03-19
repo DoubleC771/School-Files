e41dda2ae60644671efaa124bc0c2c1e28c826e8
Luc1l1us <ikazuha001@gmail.com>
2023-03-19 16:21:53 +0800

    updated SQL

--- WebDev/EcommerceWebsite/sql/logintest.sql
+++ WebDev/EcommerceWebsite/sql/logintest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2023 at 06:01 PM
+-- Generation Time: Mar 19, 2023 at 09:21 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
