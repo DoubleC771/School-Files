4758661601c3d0fd7893275ccc5e3ed516fb1adc
Luc1l1us <ikazuha001@gmail.com>
2023-05-24 19:05:44 +0800

    updated sql file

--- WebDev/EcommerceWebsite/sql/logintest.sql
+++ WebDev/EcommerceWebsite/sql/logintest.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2023 at 08:32 AM
+-- Generation Time: May 24, 2023 at 01:05 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,9 +42,9 @@ CREATE TABLE `bikeorder` (
 --
 
 INSERT INTO `bikeorder` (`OrderID`, `BikeName`, `Quantity`, `Image`, `Price`, `Description`, `Stocks`) VALUES
-(1, '2023 Canyon Aeroad CF SLX 7 Disc Etap', 0, 'Bike1.JPG', '262505', 'Experience the incredible speed of a road race bike with world-class aerodynamics. With a newly developed, super-clean aero frame and integrated cockpit, this bike saves watts every time you turn the crank, so you can go faster and attack the field even h', 50),
-(3, '2023 Canyon LUX World Cup CF 7', 0, 'bikeshop2.png', ' 305000', 'Instant acceleration, high top speed, precise handling: the Lux World Cup 7 comes into its own on techy climbs and is lightning fast when you’re sprinting to the line. Ready to race? Then you just might have found your perfect bike.', 50),
-(4, '2023 Canyon Exceed CF 7', 0, 'Bike3.JPG', ' 136695', 'It’s even quicker than it looks! The fast and strong Exceed CF 7, with its light frame and unbeatable component spec, is a thoroughbred carbon race hardtail and expects to be ridden like one.', 50),
+(1, '2023 Canyon Aeroad CF SLX 7 Disc Etap', 0, 'Bike1.JPG', '262505', 'Experience the incredible speed of a road race bike with world-class aerodynamics. With a newly developed, super-clean aero frame and integrated cockpit, this bike saves watts every time you turn the crank, so you can go faster and attack the field even h', 0),
+(3, '2023 Canyon LUX World Cup CF 7', 0, 'bikeshop2.png', ' 305000', 'Instant acceleration, high top speed, precise handling: the Lux World Cup 7 comes into its own on techy climbs and is lightning fast when you’re sprinting to the line. Ready to race? Then you just might have found your perfect bike.', 40),
+(4, '2023 Canyon Exceed CF 7', 0, 'Bike3.JPG', ' 136695', 'It’s even quicker than it looks! The fast and strong Exceed CF 7, with its light frame and unbeatable component spec, is a thoroughbred carbon race hardtail and expects to be ridden like one.', 25),
 (5, 'Factor O2 Carbon Road Frameset', 0, 'bikeshop3.png', '147690', 'Lightweight all-round racing bike', 50),
 (6, 'S-Works SL7 frameset', 0, 'bikeshop4.png', '345000', 'Why should you be forced to choose between aerodynamics and weight, between ride quality and speed? It is simple, you should not. Enter the new Tarmac climb on the lightest bike the UCI allows, then descend on the fastest. ', 50),
 (7, 'Canyon Torque CF 8', 0, 'bike6.png', ' 243360', 'Bike park laps or enduro trails? 29 or 27.5 inch wheels? With the brand-new Torque CF 8, get the best of all worlds thanks to mullet wheel sizing and a progressive, highly-adjustable geometry.', 50);
@@ -56,7 +56,8 @@ INSERT INTO `bikeorder` (`OrderID`, `BikeName`, `Quantity`, `Image`, `Price`, `D
 --
 
 CREATE TABLE `cart` (
-  `OrderID` int(255) NOT NULL,
+  `CartID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
   `BikeName` varchar(255) NOT NULL,
   `Price` varchar(255) NOT NULL,
   `Image` varchar(255) NOT NULL,
@@ -88,8 +89,8 @@ CREATE TABLE `logintable` (
 INSERT INTO `logintable` (`CustomerID`, `firstname`, `lastname`, `address`, `city`, `username`, `password`, `UserType`, `Image`) VALUES
 (3, 'deez', 'nutty', 'idk which', 'city', 'deez', '$2y$10$awVaHFXB/HWgmyaT.LlVyu4j2Yx7R9BNmjIEKMtrFx/QhWJLammX6', 1, ''),
 (5, 'mama', 'mama', 'adress', 'city', 'mama', '$2y$10$7YmkDkmNqczME.Ab6OVB9eacARDD6erG62k0woelzdCMezSgppxo6', 1, ''),
-(7, 'deez', 'deez', 'deez', 'deez', 'deez', '$2y$10$064yvA5P/VJOYJzrKJkZIuLu4DyrgAtdcMzfZcmV2KupYyE34Oms2', 0, ''),
-(8, 'deez', 'deez', 'deez', 'deez', 'deez', '$2y$10$064yvA5P/VJOYJzrKJkZIuLu4DyrgAtdcMzfZcmV2KupYyE34Oms2', 0, '');
+(7, 'yikes', 'yikes', 'yikes', 'yikes', 'yikes', '$2y$10$yg4SdEnBMaoYO3rDv6pNYOCPcqdyTUffMdgKdZ5SjYolsnWgF0Flm', 0, ''),
+(8, 'yikes', 'yikes', 'yikes', 'yikes', 'yikes', '$2y$10$yg4SdEnBMaoYO3rDv6pNYOCPcqdyTUffMdgKdZ5SjYolsnWgF0Flm', 0, '');
 
 -- --------------------------------------------------------
 
@@ -108,6 +109,28 @@ CREATE TABLE `salesreport` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `salesreport`
+--
+
+INSERT INTO `salesreport` (`SalesID`, `BikeName`, `Price`, `Quantity`, `Image`, `date`, `time`) VALUES
+(4, 'Canyon Torque CF 8', ' 243360', 1000, 'bike6.png', 'Tue May 23 2023', '1:16:15'),
+(5, 'Canyon Torque CF 8', ' 243360', 500, 'bike6.png', 'Tue May 23 2023', '1:17:32'),
+(6, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 30, 'Bike1.JPG', 'Wed May 24 2023', '18:19:36'),
+(7, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(8, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(9, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(10, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(11, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(12, '2023 Canyon Exceed CF 7', ' 136695', 1, 'Bike3.JPG', 'CurrentTime', 'CurrentDate'),
+(13, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 50, 'Bike1.JPG', 'Wed May 24 2023', '18:32:39'),
+(14, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 1, 'Bike1.JPG', 'Wed May 24 2023', '18:34:46'),
+(15, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 1, 'Bike1.JPG', 'Wed May 24 2023', '18:41:46'),
+(16, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 49, 'Bike1.JPG', 'Wed May 24 2023', '18:42:11'),
+(17, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 1, 'Bike1.JPG', 'Wed May 24 2023', '18:44:36'),
+(18, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 50, 'Bike1.JPG', 'Wed May 24 2023', '18:49:7'),
+(19, '2023 Canyon Aeroad CF SLX 7 Disc Etap', '262505', 50, 'Bike1.JPG', 'Wed May 24 2023', '18:52:48');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -121,7 +144,7 @@ ALTER TABLE `bikeorder`
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`OrderID`);
+  ADD PRIMARY KEY (`CartID`);
 
 --
 -- Indexes for table `logintable`
@@ -149,7 +172,7 @@ ALTER TABLE `bikeorder`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `OrderID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `logintable`
@@ -161,7 +184,7 @@ ALTER TABLE `logintable`
 -- AUTO_INCREMENT for table `salesreport`
 --
 ALTER TABLE `salesreport`
-  MODIFY `SalesID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `SalesID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
